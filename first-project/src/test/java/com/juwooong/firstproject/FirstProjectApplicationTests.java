609383d9aa8d7e7d009226a9fa2ae49e4f828e1d
Juwoong <wwong7247@gmail.com>
2022-04-19 18:29:02 +0900

    Inital Commit

---
+++ first-project/src/test/java/com/juwooong/firstproject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwooong.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
